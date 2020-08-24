ca64bba3c6879c59c118b99dc195e8743d94a16d
Gabriel Romero <gabester78@gmail.com>
2020-08-24 15:56:40 -0500

    Basic intro done.

---
+++ conways-game-of-life/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Intro from "./components/Intro";
+import About from "./components/About";
+
+function App() {
+  return (
+    <div className="App">
+      <Intro />
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ conways-game-of-life/src/components/Intro.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <h1>Welcome to Conway's Game of Life!</h1>
+    </div>
+  );
+};
+
+export default Intro;
